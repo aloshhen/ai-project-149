b369a660531cd259757378cbcd7318c24a42c844
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-01-14 23:50:47 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          950: '#020617',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
